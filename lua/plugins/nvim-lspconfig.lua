a38bdb5a6a8e56dcfd8f041b7c93242a2f709634
serchugar <chugarsergio@gmail.com>
2024-10-16 13:45:00 +0200

    add mason and nvim-lspconfig for Lua, Cpp, Java

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,64 @@
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"folke/neodev.nvim",
+	},
+	config = function()
+		vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+		vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+		vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+		vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+		local on_attach = function(_, bufnr)
+			vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
+			local opts = { buffer = bufnr }
+			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+			vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+			vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+			vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+			vim.keymap.set('n', '<space>wl', function()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end, opts)
+			vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+			vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+			vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+			vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+			vim.keymap.set('n', '<space>f', function()
+				vim.lsp.buf.format { async = true }
+			end, opts)
+		end
+
+		require("neodev").setup()
+
+		-- LUA
+		require("lspconfig").lua_ls.setup({
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					telemetry = { enable = false },
+					workspace = { checkThirdParty = false },
+				}
+			}
+		})
+
+		-- C++
+		-- Wasn't able to make clangd include my local include path containing <bits/stdc++.h>
+		-- I've tried so many times and different ways that I'm sick of it.
+		-- So for now, if you want to use any extra header file you have. Just copy-paste it into
+		-- "C:\Users\<Username>\AppData\Local\nvim-data\mason\packages\clangd\clangd_19.1.0\lib\clang\19\include"
+		require("lspconfig").clangd.setup({
+			on_attach = on_attach,
+		})
+
+		-- JAVA
+		require("lspconfig").jdtls.setup({
+			on_attach = on_attach,
+		})
+
+	end
+}
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,4 +5,23 @@ return {
   opts = {},
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  config = function()
+		require("oil").setup({
+			view_options = {
+
+				-- Show files and directories that start with "."
+    			show_hidden = true,
+
+				-- This function defines what is considered a "hidden" file
+    			is_hidden_file = function(name, bufnr)
+      				return vim.startswith(name, ".")
+    			end,
+
+				-- This function defines what will never be shown, even when `show_hidden` is set
+    			is_always_hidden = function(name, bufnr)
+      				return false
+    			end,
+			},
+		})
+	end,
 }
