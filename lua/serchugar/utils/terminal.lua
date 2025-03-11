e1d8c4faf12cb199c5889cc94c263d69ba9fe8f6
serchugar <chugarsergio@gmail.com>
2025-03-11 11:58:48 +0100

    fix terminal.lua: remove unnecessary variable, print, and cls
    Removed local variable winId, can be obtained from the bufnr.
    Removed forgotten print statement.
    Fixed cls command from executing everytime an existing terminal window
    opened.

--- lua/serchugar/utils/terminal.lua
+++ lua/serchugar/utils/terminal.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 local bufnr = nil
-local winId = nil
 
 -- TODO: Make position and size update on autocmd VimResize
 local width = math.floor(vim.o.columns * 0.7)
@@ -20,22 +19,21 @@ local opts = {
 function M.OpenFloat()
     if not bufnr or not vim.api.nvim_buf_is_valid(bufnr) then
         bufnr = vim.api.nvim_create_buf(false, true)
-        winId = vim.api.nvim_open_win(bufnr, true, opts)
+        vim.api.nvim_open_win(bufnr, true, opts)
 
         vim.api.nvim_command('term')
         vim.fn.chansend(vim.api.nvim_get_option_value("channel", { buf = bufnr }), "cls\r")
     else
-        winId = vim.api.nvim_open_win(bufnr, true, opts)
-
-        vim.fn.chansend(vim.api.nvim_get_option_value("channel", { buf = bufnr }), "cls\r")
+        vim.api.nvim_open_win(bufnr, true, opts)
     end
     vim.api.nvim_command('startinsert')
 end
 
 function M.Hide()
+
     bufnr = vim.api.nvim_get_current_buf()
     if vim.bo[bufnr].buftype == "terminal" then
-        print(bufnr)
+        local winId = vim.fn.bufwinid(bufnr)
         vim.api.nvim_win_hide(winId)
     end
 end
