0da4cf09678d5b3b97124da5820a63ddc146629d
serchugar <chugarsergio@gmail.com>
2025-03-11 14:23:35 +0100

    add nvim-colorizer

---
+++ lua/serchugar/plugins/nvim-colorizer.lua
@@ -0,0 +1,10 @@
+return {
+    "catgoose/nvim-colorizer.lua",
+    config = function()
+        require("colorizer").setup{
+            user_default_options = {
+                names = false,
+            },
+        }
+    end,
+}
