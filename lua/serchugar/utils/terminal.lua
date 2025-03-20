ecb3071b38ec6e39b7c3cd90f000a2adaef19d8b
serchugar <chugarsergio@gmail.com>
2025-03-20 18:43:36 +0100

    fix terminal.lua bug where open/closing float term window in already a regular term window raised an error

--- lua/serchugar/utils/terminal.lua
+++ lua/serchugar/utils/terminal.lua
@@ -1,7 +1,10 @@
+-- Own custom floating terminal module
+-- Functions used in mappings.lua
+
 local M = {}
 
 local bufnr = nil
-local opts = nil
+local floatWindowOpts = nil
 
 local function get_float_opts()
     local width = math.floor(vim.o.columns * 0.8)
@@ -18,15 +21,16 @@ local function get_float_opts()
 end
 
 function M.OpenFloat()
-    opts = get_float_opts()
+    floatWindowOpts = get_float_opts()
     if not bufnr or not vim.api.nvim_buf_is_valid(bufnr) then
         bufnr = vim.api.nvim_create_buf(false, true)
-        vim.api.nvim_open_win(bufnr, true, opts)
+        vim.api.nvim_open_win(bufnr, true, floatWindowOpts)
 
         vim.api.nvim_command('term')
         vim.fn.chansend(vim.api.nvim_get_option_value("channel", { buf = bufnr }), "cls\r")
-    else
-        vim.api.nvim_open_win(bufnr, true, opts)
+
+    elseif vim.api.nvim_get_current_buf() ~= bufnr then
+        vim.api.nvim_open_win(bufnr, true, floatWindowOpts)
     end
     vim.api.nvim_command('startinsert')
 end
@@ -35,8 +39,18 @@ function M.Hide()
     local currentBufnr = vim.api.nvim_get_current_buf()
     if vim.bo[currentBufnr].buftype == "terminal" then
         local winId = vim.fn.bufwinid(currentBufnr)
-        if winId ~= -1 then -- =1 means buffer is not displayed in any window
-            vim.api.nvim_win_hide(winId)
+        if winId ~= -1 then -- ==-1 means buffer is not displayed in any window
+            if vim.api.nvim_win_get_config(winId).relative ~= '' then -- If relative is not empty means its floating
+                vim.api.nvim_win_hide(winId)
+
+                -- Exit insert mode of beneath term if float term window is hidden on top of another regular terminal window
+                local key = vim.api.nvim_replace_termcodes('<C-\\><C-n>', true, true, true)
+                vim.api.nvim_feedkeys(key, 't', false)
+            else
+                -- Exit insert mode instead of hidding if term window is not floating
+                local key = vim.api.nvim_replace_termcodes('<C-\\><C-n>', true, true, true)
+                vim.api.nvim_feedkeys(key, 't', false)
+            end
         end
     end
 end
@@ -45,8 +59,8 @@ local function ResizeFloat()
     if bufnr and vim.api.nvim_buf_is_valid(bufnr) then
         local winId = vim.fn.bufwinid(bufnr)
         if winId ~= -1 then
-            opts = get_float_opts()
-            vim.api.nvim_win_set_config(winId, opts)
+            floatWindowOpts = get_float_opts()
+            vim.api.nvim_win_set_config(winId, floatWindowOpts)
         end
     end
 end
